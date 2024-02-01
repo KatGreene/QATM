ac50da7a474dcb669f4ebe218847916c870f1e4c
KatGreene <124125678+KatGreene@users.noreply.github.com>
2024-02-01 23:22:42 +0800

    Add files via upload

--- QATM.py
+++ QATM.py
@@ -19,7 +19,7 @@ import bpy
 bl_info = {
     "name": "QATM",
     "author": "绿毛猫KatGreene",
-    "version": (0, 1),
+    "version": (0, 2),
     "blender": (3, 6, 0),
     "location": "View3D > Toolbar",
     "description": "Quick Apply Tagged Material 快速应用含同名材质",
@@ -66,11 +66,11 @@ class OBJECT_OT_AddOutlineButton(bpy.types.Operator):
 
 ### 删除选中物体的所有名为"000OutLine"的几何节点修改器
 def delete_geometry_node_modifiers(named):
-    for obj in bpy.context.selected_objects:  # 遍历场景中的所有物体
+    for obj in bpy.context.selected_objects:  # 遍历场景中的所有选中物体
         # 确保我们只对网格类型的物体进行操作
         if obj.type == 'MESH':
             # 遍历物体的修改器
-            modifiers_to_remove = [mod for mod in obj.modifiers if mod.type == 'NODES' and mod.name == named]
+            modifiers_to_remove = [mod for mod in obj.modifiers if mod.type == 'NODES' and named in mod.name]
             for modifier in modifiers_to_remove:
                 obj.modifiers.remove(modifier)
 
@@ -129,104 +129,120 @@ def copy_material_to_selected_objects(mat_name):
                 material_to_copy = mat
                 break
 
-    # 应用材质
-    for obj in selected_objects:
-        # 如果没有材质，则新建材质
-        if len(obj.material_slots) == 0:
-            new_mat = bpy.data.materials.new(name="00Midl")
-            obj.data.materials.append(new_mat)
-            bpy.data.materials.remove(new_mat)
-            
-        if obj.type == 'MESH' and len(obj.material_slots) > 0:
-            # 获取原材质中图像纹理所关联的图像
-            original_image_textures = {}
-            for mat_slot in obj.material_slots:
-                if mat_slot.material and mat_slot.material.use_nodes:
-                    for node in mat_slot.material.node_tree.nodes:
-                        if node.type == 'TEX_IMAGE':
-                            original_image_textures[node.location.y] = node.image
-    
-            #复制和更新图像
-            if material_to_copy: #and original_image_textures
-                new_mat = material_to_copy.copy()
-                new_mat.name += "_" + obj.name
-                obj.data.materials[0] = new_mat
+    # 存储材质名称和材质的映射，避免创建不必要的副本
+    material_mapping = {}
 
-                # 按照节点树Y方向位置顺序排序
-                sorted_images = [image for key, image in sorted(original_image_textures.items(), reverse=True)]
-                if sorted_images:  # 防止除数为0
-                    # 用原图像替换新图像
-                    i = 0
-                    for node in new_mat.node_tree.nodes:
-                        if node.type == 'TEX_IMAGE':
-                            index = i % len(sorted_images)
-                            node.image = sorted_images[index]
-                            i += 1
+    for obj in selected_objects:
+        if obj.type == 'MESH':
+            # 如果没有材质，则新建材质
+            if len(obj.material_slots) == 0:
+                new_mat = bpy.data.materials.new(name=obj.name)
+                obj.data.materials.append(new_mat)
+            
+            if obj.type == 'MESH' and len(obj.material_slots) > 0:
+                # 获取原材质中图像纹理所关联的图像
+                original_image_textures = {}
+                for mat_slot in obj.material_slots:
+                    if mat_slot.material and mat_slot.material.use_nodes:
+                        for node in mat_slot.material.node_tree.nodes:
+                            if node.type == 'TEX_IMAGE':
+                                original_image_textures[node.location.y] = node.image
+        
+                # 仅当有要复制的材料时进行复制
+                if material_to_copy:
+                # 为每个唯一的材质数据块创建一个材料映射关系
+                    for mat_slot in obj.material_slots:
+                        if mat_slot.material:
+                            original_mat_name = mat_slot.material.name
+                            # 检查映射中是否已经存在这个材质data的副本
+                            if original_mat_name in material_mapping:
+                                # 使用已经创建的副本
+                                new_mat = material_mapping[original_mat_name]
+                            else:
+                                # 创建新的副本，并更新映射
+                                new_mat = material_to_copy.copy()
+
+                            # 处理原有材料名称
+                            processed_original_name = remove_keywords(original_mat_name, names_to_check)
+                            new_mat.name = f'{material_to_copy.name}_{processed_original_name}'
+
+                            material_mapping[original_mat_name] = new_mat
+
+                    obj.data.materials[0] = new_mat
+
+                    # 按照节点树Y方向位置顺序排序
+                    sorted_images = [image for key, image in sorted(original_image_textures.items(), reverse=True)]
+                    if sorted_images:  # 防止除数为0
+                        # 用原图像替换新图像
+                        i = 0
+                        for node in new_mat.node_tree.nodes:
+                            if node.type == 'TEX_IMAGE':
+                                index = i % len(sorted_images)
+                                node.image = sorted_images[index]
+                                i += 1
+
+# 声明一个函数用于处理原名称
+def remove_keywords(original_name, keywords):
+    new_name = original_name
+    for keyword in keywords:
+        # 如果关键词存在于原材料名称
+        if keyword in new_name:
+            # 找到关键词后面的部分（包括关键词本身）
+            key_and_trailing = f'{keyword}_'
+            # 用替换方法移除关键词和它后面的下划线
+            new_name = new_name.replace(key_and_trailing, '')
+    return new_name
 
 #UI操作类
-class MATERIAL_OT_copy_material_01Skin(bpy.types.Operator):
-    """复制包含'01Skin'的材质"""
+class BaseCopyMaterialOperator(bpy.types.Operator):
+    """基础复制材质操作类"""
+    bl_idname = "material.copy_base"
+    bl_label = "复制基础材质"
+    material_suffix = ""
+
+    def execute(self, context):
+        copy_material_to_selected_objects(self.material_suffix)
+        return {'FINISHED'}
+
+class MATERIAL_OT_copy_material_01Skin(BaseCopyMaterialOperator):
     bl_idname = "material.copy_material_01skin"
     bl_label = "01Skin 皮肤"
-    def execute(self, context):
-        copy_material_to_selected_objects("01Skin")
-        return {'FINISHED'}
+    material_suffix = "01Skin"
 
-class MATERIAL_OT_copy_material_02Hair(bpy.types.Operator):
-    """复制包含'02Hair'的材质"""
+class MATERIAL_OT_copy_material_02Hair(BaseCopyMaterialOperator):
     bl_idname = "material.copy_material_02hair"
     bl_label = "02Hair 头发"
-    def execute(self, context):
-        copy_material_to_selected_objects("02Hair")
-        return {'FINISHED'}
-    
-class MATERIAL_OT_copy_material_03CloS(bpy.types.Operator):
-    """复制包含'03CloS'的材质"""
+    material_suffix = "02Hair"
+
+class MATERIAL_OT_copy_material_03CloS(BaseCopyMaterialOperator):
     bl_idname = "material.copy_material_03clos"
     bl_label = "03CloS 光滑衣物"
-    def execute(self, context):
-        copy_material_to_selected_objects("03CloS")
-        return {'FINISHED'}
+    material_suffix = "03CloS"
 
-class MATERIAL_OT_copy_material_04CloR(bpy.types.Operator):
-    """复制包含'04CloR'的材质"""
+class MATERIAL_OT_copy_material_04CloR(BaseCopyMaterialOperator):
     bl_idname = "material.copy_material_04clor"
     bl_label = "04CloR 粗糙衣物"
-    def execute(self, context):
-        copy_material_to_selected_objects("04CloR")
-        return {'FINISHED'}
-    
-class MATERIAL_OT_copy_material_05Metl(bpy.types.Operator):
-    """复制包含'05Metl'的材质"""
+    material_suffix = "04CloR"
+
+class MATERIAL_OT_copy_material_05Metl(BaseCopyMaterialOperator):
     bl_idname = "material.copy_material_05metl"
     bl_label = "05Metl 金属"
-    def execute(self, context):
-        copy_material_to_selected_objects("05Metl")
-        return {'FINISHED'}
-    
-class MATERIAL_OT_copy_material_06Eyes(bpy.types.Operator):
-    """复制包含'06Eyes'的材质"""
+    material_suffix = "05Metl"
+
+class MATERIAL_OT_copy_material_06Eyes(BaseCopyMaterialOperator):
     bl_idname = "material.copy_material_06eyes"
     bl_label = "06Eyes 眼睛"
-    def execute(self, context):
-        copy_material_to_selected_objects("06Eyes")
-        return {'FINISHED'}
-    
-class MATERIAL_OT_copy_material_07Stks(bpy.types.Operator):
-    """复制包含'07Stks'的材质"""
+    material_suffix = "06Eyes"
+
+class MATERIAL_OT_copy_material_07Stks(BaseCopyMaterialOperator):
     bl_idname = "material.copy_material_07stks"
     bl_label = "07Stks 丝袜"
-    def execute(self, context):
-        copy_material_to_selected_objects("07Stks")
-        return {'FINISHED'}
-    
-class MATERIAL_OT_copy_material_08Cstm(bpy.types.Operator):
-    """复制包含'08Cstm'的材质"""
+    material_suffix = "07Stks"
+
+class MATERIAL_OT_copy_material_08Cstm(BaseCopyMaterialOperator):
     bl_idname = "material.copy_material_08cstm"
     bl_label = "08Cstm 自定义"
-    def execute(self, context):
-        copy_material_to_selected_objects("08Cstm")
-        return {'FINISHED'}
+    material_suffix = "08Cstm"
 
 
 
@@ -244,6 +260,7 @@ def delete_unused_materials_by_name(names):
             if any(name in material.name for name in names):
                 # 删除材质
                 bpy.data.materials.remove(material)
+    
 
 # 运算符，执行删除操作
 class WM_OT_DeleteUnusedMaterialsByName(bpy.types.Operator):
@@ -273,80 +290,102 @@ class MATERIAL_PT_custom_panel(bpy.types.Panel):
         scene = context.scene
         mat_assoc_props = scene.mat_association_props
         
+        layout.use_property_split = False
+        layout.use_property_decorate = False
+        
         # 绘制集合选择框
-        row = layout.row()
         layout.label(text="指定优先搜索范围")
         layout.prop(mat_assoc_props, "collection")
 
         layout.label(text="匹配名称含同样 xxXXXX 的材质")
-        row = layout.row()
-        row.operator("material.copy_material_01skin", icon="SHADING_SOLID")
-        row = layout.row()
-        row.operator("material.copy_material_02hair", icon="MATSPHERE")
-        row = layout.row()
-        row = layout.row(align=True)
-        row.operator("material.copy_material_03clos", icon="SHADING_RENDERED")
-        row = layout.row()
-        row.operator("material.copy_material_04clor", icon="SHADING_TEXTURE")
-        row = layout.row()
-        row = layout.row(align=True)
-        row.operator("material.copy_material_05metl", icon="NODE_MATERIAL")
-        row = layout.row()
-        row.operator("material.copy_material_06eyes", icon="ANTIALIASED")
-        row = layout.row()
-        row.operator("material.copy_material_07stks", icon="PROP_ON")
-        row = layout.row(align=True)
-        row = layout.row()
+
+        box = layout.box()
+        #box.label(text="基础", icon='LIGHT_DATA')
+        col = box.column()
+        col.operator("material.copy_material_01skin", icon="SHADING_SOLID")
+        col.operator("material.copy_material_02hair", icon="MATSPHERE")
+        
+        
+        box = layout.box()
+        #box.label(text="衣物", icon='LIGHT_DATA')
+        col = box.column()
+        col.operator("material.copy_material_03clos", icon="SHADING_RENDERED")
+        col.operator("material.copy_material_04clor", icon="SHADING_TEXTURE")
+        
+        box = layout.box()
+        #box.label(text="其它", icon='LIGHT_DATA')
+        col = box.column()
+        col.operator("material.copy_material_05metl", icon="NODE_MATERIAL")
+        col.operator("material.copy_material_06eyes", icon="ANTIALIASED")
+        col.operator("material.copy_material_07stks", icon="PROP_ON")
+        
+        box = layout.box()
+        row = box.row()
         row.operator("material.copy_material_08cstm", icon="SEQ_CHROMA_SCOPE")
-        row = layout.row()
+        
+class MATERIAL_PT_custom_subpanel01(bpy.types.Panel):
+    """创建一个在N键属性栏中的自定义面板"""
+    bl_label = "删除多余材质"
+    bl_parent_id = "MATERIAL_PT_custom_panel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'QATM'
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+
         layout.label(text="删除名称含 xxXXXX 的多余材质")
         row = layout.row()
         row.scale_y = 1.5
         button = row.operator("wm.delete_unused_materials_by_name", text="删除多余对应材质", icon="TRASH")
-        row = layout.row()
-        row = layout.row()
+
+class MATERIAL_PT_custom_subpanel02(bpy.types.Panel):
+    """创建一个在N键属性栏中的自定义面板"""
+    bl_label = "描边"
+    bl_parent_id = "MATERIAL_PT_custom_panel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'QATM' 
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+
         layout.label(text="几何节点自适应描边")
         row = layout.row()
         row.scale_y = 1.5
         button = row.operator("object.add_outline_button", text="添加描边", icon="ADD")
         row.operator("wm.delete_geometry_nodes_by_name", text="删除描边", icon="X")
-        row = layout.row()
         
 
-def register():
-    bpy.utils.register_class(MATERIAL_OT_copy_material_01Skin)
-    bpy.utils.register_class(MATERIAL_OT_copy_material_02Hair)
-    bpy.utils.register_class(MATERIAL_OT_copy_material_03CloS)
-    bpy.utils.register_class(MATERIAL_OT_copy_material_04CloR)
-    bpy.utils.register_class(MATERIAL_OT_copy_material_05Metl)
-    bpy.utils.register_class(MATERIAL_OT_copy_material_06Eyes)
-    bpy.utils.register_class(MATERIAL_OT_copy_material_07Stks)
-    bpy.utils.register_class(MATERIAL_OT_copy_material_08Cstm)
-    bpy.utils.register_class(OBJECT_OT_AddOutlineButton)
-    bpy.utils.register_class(WM_OT_DeleteUnusedMaterialsByName)
-    bpy.utils.register_class(WM_OT_DeleteGeometryNodesByName)
+classes = (
+    MATERIAL_OT_copy_material_01Skin,
+    MATERIAL_OT_copy_material_02Hair,
+    MATERIAL_OT_copy_material_03CloS,
+    MATERIAL_OT_copy_material_04CloR,
+    MATERIAL_OT_copy_material_05Metl,
+    MATERIAL_OT_copy_material_06Eyes,
+    MATERIAL_OT_copy_material_07Stks,
+    MATERIAL_OT_copy_material_08Cstm,
+    OBJECT_OT_AddOutlineButton,
+    WM_OT_DeleteUnusedMaterialsByName,
+    WM_OT_DeleteGeometryNodesByName,
+    MaterialAssociationProperties,
+    MATERIAL_PT_custom_panel,
+    MATERIAL_PT_custom_subpanel01,
+    MATERIAL_PT_custom_subpanel02,
+)
 
-    bpy.utils.register_class(MaterialAssociationProperties)
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
     
     bpy.types.Scene.mat_association_props = bpy.props.PointerProperty(type=MaterialAssociationProperties)
 
-    bpy.utils.register_class(MATERIAL_PT_custom_panel)
-
 def unregister():
-    bpy.utils.unregister_class(MATERIAL_OT_copy_material_01Skin)
-    bpy.utils.unregister_class(MATERIAL_OT_copy_material_02Hair)
-    bpy.utils.unregister_class(MATERIAL_OT_copy_material_03CloS)
-    bpy.utils.unregister_class(MATERIAL_OT_copy_material_04CloR)
-    bpy.utils.unregister_class(MATERIAL_OT_copy_material_05Metl)
-    bpy.utils.unregister_class(MATERIAL_OT_copy_material_06Eyes)
-    bpy.utils.unregister_class(MATERIAL_OT_copy_material_07Stks)
-    bpy.utils.unregister_class(MATERIAL_OT_copy_material_08Cstm)
-    bpy.utils.unregister_class(OBJECT_OT_AddOutlineButton)
-    bpy.utils.unregister_class(WM_OT_DeleteUnusedMaterialsByName)
-    bpy.utils.unregister_class(WM_OT_DeleteGeometryNodesByName)
-    bpy.utils.unregister_class(MATERIAL_PT_custom_panel)
-
-    bpy.utils.unregister_class(MaterialAssociationProperties)
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
     
     del bpy.types.Scene.mat_association_props
 
